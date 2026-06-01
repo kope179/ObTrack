bc1b1654a0f8777b65fb417e88c924df5ae488e7
kope179 <kopbluts@gmail.com>
2026-06-01 10:06:26 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1000,
+    minHeight: 600,
+    title: 'OBTrack',
+    webPreferences: { nodeIntegration: false }
+  })
+  win.loadFile('trade-journal.html')
+  win.setMenuBarVisibility(false)
+}
+
+app.whenReady().then(createWindow)
+app.on('window-all-closed', () => app.quit())
